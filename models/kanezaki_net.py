f2524f7caba44b26b6c464d0c37c00619d902232
obarads <b1510803@planet.kanazawa-it.ac.jp>
2019-07-11 03:59:15 +0900

    update

--- models/kanezaki_net.py
+++ models/kanezaki_net.py
@@ -11,6 +11,16 @@ class KanezakiNet(chainer.Chain):
         super(KanezakiNet, self).__init__()
         with self.init_scope():
             self.conv_b1 = ConvBlock(input_dim,nChannel,ksize=3,stride=1,pad=1,use_bn=True)
-            for 
-            self.conv_b2 = ConvBlock(nChannel, )
+            self.conv_b2 = []
+            for i in range(nConv-1):
+                self.conv_b2.append(ConvBlock(nChannel, nChannel, ksize=3, stride=1, pad=1, use_bn=True))
+            self.conv_b3 = ConvBlock(nChannel, nChannel, ksize=1, stride=1, pad=0, use_bn=True)
+            self.nChannel = nChannel
+            self.nConv = nConv
 
+    def __call__(self,x,y):
+        h = self.conv_b1(x)
+        for i in range(self.nConv-1):
+            h = self.conv_b2[i](h)
+        h = self.conv_b3(h)
+        return h
